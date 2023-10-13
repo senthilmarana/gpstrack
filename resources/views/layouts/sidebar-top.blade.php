677eda6737dd45d7b9bc9e3f600707942d6cf20f
senthilmarana <senthilmaran.a@sproudwings.in>
2023-10-13 18:57:52 +0530

    side bar update

--- resources/views/layouts/sidebar-top.blade.php
+++ resources/views/layouts/sidebar-top.blade.php
@@ -33,6 +33,22 @@
                 </li>
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bx-data"></i>
+                        <span key="t-stock">Stock</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="javascript: void(0);" key="t-device">Device</a></li>
+                        <li><a href="javascript: void(0);" key="t-sim">Sim</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bx-layer"></i>
+                        <span key="t-licence">Licence</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
                         <i class="bx bx-file"></i>
                         <span key="t-multi-level">Reports</span>
                     </a>
